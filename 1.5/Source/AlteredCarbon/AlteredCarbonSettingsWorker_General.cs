782ff59c0c03d517df6ac91da1c2cc38d1fe15bd
Kieran David Evans <keyz182@gmail.com>
2025-03-15 12:19:08 +0000

    Add single-use mental fuses with optional explosion effect
    Implemented functionality to "blow" single-use mental fuses, removing them after triggering, with an optional visual explosion effect. Added related settings, translations, and updated references in the project configuration.

--- 1.5/Source/AlteredCarbon/AlteredCarbonSettingsWorker_General.cs
+++ 1.5/Source/AlteredCarbon/AlteredCarbonSettingsWorker_General.cs
@@ -13,11 +13,15 @@ namespace AlteredCarbon
     {
         public bool enableStackSpawning = true;
         public bool sleeveDeathDoesNotCauseGearTainting = true;
+        public bool singleUseMentalFuses = true;
+        public bool singleUseMentalFusePop = true;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref enableStackSpawning, "enableStackSpawning", true);
             Scribe_Values.Look(ref sleeveDeathDoesNotCauseGearTainting, "sleeveDeathDoesNotCauseGearTainting", true);
+            Scribe_Values.Look(ref singleUseMentalFuses, "singleUseMentalFuses", true);
+            Scribe_Values.Look(ref singleUseMentalFusePop, "singleUseMentalFusePop", true);
         }
 
         public override void CopyFrom(PatchOperationWorker savedWorker)
@@ -25,18 +29,24 @@ namespace AlteredCarbon
             var copy = savedWorker as AlteredCarbonSettingsWorker_General;
             this.enableStackSpawning = copy.enableStackSpawning;
             this.sleeveDeathDoesNotCauseGearTainting = copy.sleeveDeathDoesNotCauseGearTainting;
+            this.singleUseMentalFuses = copy.singleUseMentalFuses;
+            this.singleUseMentalFusePop = copy.singleUseMentalFusePop;
         }
 
         public override void DoSettings(ModSettingsContainer container, Listing_Standard list)
         {
             DoCheckbox(list, "AC.EnableStackSpawning".Translate(), ref enableStackSpawning, "AC.EnableStackSpawningDesc".Translate());
             DoCheckbox(list, "AC.SleeveDeathDoesNotCauseGearTainting".Translate(), ref sleeveDeathDoesNotCauseGearTainting, null);
+            DoCheckbox(list, "AC.SingleUseMentalFuses".Translate(), ref singleUseMentalFuses, "AC.SingleUseMentalFusesDesc".Translate());
+            DoCheckbox(list, "AC.SingleUseMentalFusePop".Translate(), ref singleUseMentalFusePop, null);
         }
 
         public override void Reset()
         {
             enableStackSpawning = true;
             sleeveDeathDoesNotCauseGearTainting = true;
+            singleUseMentalFuses = true;
+            singleUseMentalFusePop = true;
         }
     }
 }
